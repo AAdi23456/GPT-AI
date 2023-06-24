bf9f1e4bd1cdf0c76ecc9b74135ac2a9b5168e90
Aaditya Kumar Jha <118712556+AAdi23456@users.noreply.github.com>
2023-06-24 18:23:36 +0530

    Update openai.js

--- routes/openai.js
+++ routes/openai.js
@@ -48,7 +48,7 @@ AIroutes.post('/questions',Validate, async (req, res) => {
 
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `provide me  random  questions related to ${tech}  which can be either easy,moderate or tough.
+            prompt: `provide me a unique random  question everytime i ask you related to ${tech}  .
         `,
             temperature: 0,
             max_tokens: 3000,
